35ff8d7b348761e6b8c3f9ce5faa84ae2413eef8
Kasper Siig <kasper@siig.tech>
2026-08-24 23:26:01 +0200

    Bump version to 0.3.1 for update test (#7)
    Co-authored-by: Kasper Siig <kasper.siig@velatir.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ksiig.tmuxmobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 4
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
